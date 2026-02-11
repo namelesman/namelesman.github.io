9414bd4d1924835e295b4d951e5c7e29c39b2f21
unknown <thiiaagomedeiros@gmail.com>
2026-02-11 14:01:33 -0300

    Update portfolio project to GTA I CSS3D and fix JSX types error

--- components/portfolio-page.tsx
+++ components/portfolio-page.tsx
@@ -7,8 +7,8 @@ export function PortfolioPage() {
       <div className="portfolio-box">
         <div className="img-box">
           <Image
-            src="/Assets/image/mycaffee.jpg"
-            alt="My Caffee project screenshot"
+            src="Assets\image\112042026-f6378900-8b3e-11eb-84a4-e4a92f86678e.jpg"
+            alt="GTA I CSS3D"
             width={600}
             height={240}
             style={{ width: "100%", height: "100%", objectFit: "cover" }}
@@ -16,22 +16,22 @@ export function PortfolioPage() {
         </div>
         <div className="info-box">
           <div className="info-tittle">
-            <h3>My Caffee</h3>
+            <h3>GTA I CSS3D</h3>
             <a href="#">
               Live Preview
-              <i className="bx bx-link-external" />
+              <i className="https://gtacss3d.netlify.app/" />
             </a>
           </div>
           <p>Tech Used:</p>
           <p>
             {
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta iure aliquam deserunt iusto dolorum nostrum quae. In, officia ipsam!"
+              "Fiz esta demo só para ver o quanto o CSS 3D é poderoso, e acabei me empolgando...Também fiquei curioso para ver se CSS serve para fazer jogos (alerta de spoiler: não serve muito)."
             }
           </p>
         </div>
         <div className="btn-box">
           <a
-            href="https://github.com/FrancisLauriano/meu_cafe"
+            href="https://github.com/namelesman/GTA-I-HTML-SCSS-JS"
             target="_blank"
             rel="noopener noreferrer"
             className="btn"
