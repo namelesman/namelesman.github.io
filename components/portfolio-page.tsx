6363df0cc282386d5f13ca3b99ae5944ed8072a0
Thiago Medeiros <135679355+namelesman@users.noreply.github.com>
2026-02-11 13:27:12 -0300

    Update project details and image in portfolio page

--- components/portfolio-page.tsx
+++ components/portfolio-page.tsx
@@ -7,8 +7,8 @@ export function PortfolioPage() {
       <div className="portfolio-box">
         <div className="img-box">
           <Image
-            src="/Assets/image/mycaffee.jpg"
-            alt="My Caffee project screenshot"
+            src="https://user-images.githubusercontent.com/1726083/112042026-f6378900-8b3e-11eb-84a4-e4a92f86678e.jpg"
+            alt="GTA I CSS 3D"
             width={600}
             height={240}
             style={{ width: "100%", height: "100%", objectFit: "cover" }}
@@ -25,13 +25,13 @@ export function PortfolioPage() {
           <p>Tech Used:</p>
           <p>
             {
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta iure aliquam deserunt iusto dolorum nostrum quae. In, officia ipsam!"
+              "Fiz esta demo só para ver o quanto o CSS 3D é poderoso, e acabei me empolgando. Também fiquei curioso para ver se CSS serve para fazer jogos (alerta de spoiler: não serve muito)."
             }
           </p>
         </div>
         <div className="btn-box">
           <a
-            href="https://github.com/FrancisLauriano/meu_cafe"
+            href="https://github.com/namelesman/GTA-I-HTML-SCSS-JS"
             target="_blank"
             rel="noopener noreferrer"
             className="btn"
