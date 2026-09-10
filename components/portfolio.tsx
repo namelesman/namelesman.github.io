ea4343ab287158bbe07ab8056e05cc68eb428d44
Thiago Medeiros <namelesman@users.noreply.github.com>
2026-09-10 16:47:50 -0300

    feat: redesign portfolio to matrix theme and restructure pages

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -1,19 +1,17 @@
 "use client"
 
 import { ProfilePage } from "./profile-page"
-import { WorkExperiencePage } from "./work-experience-page"
-import { EducationPage } from "./education-page"
-import { ServicesPage } from "./services-page"
+import { WorkAndEducationPage } from "./work-and-education-page"
 import { SkillsPage } from "./skills-page"
 import { PortfolioPage } from "./portfolio-page"
 import { ContactPage } from "./contact-page"
 import { BookInteractions } from "./book-interactions"
-import { ParticlesBackground } from "./particles-background"
+import MatrixBackground from "./MatrixBackground"
 
 export function Portfolio() {
   return (
     <>
-      <ParticlesBackground />
+      <MatrixBackground />
 
       <div className="casing">
         <div className="cover cover-left" />
@@ -28,14 +26,14 @@ export function Portfolio() {
           {/* Page 1 & 2 */}
           <div className="book-page page-right turn" id="turn-1">
             <div className="page-front">
-              <WorkExperiencePage />
+              <WorkAndEducationPage />
               <span className="number-page">1</span>
               <span className="nextprev-btn" data-page="turn-1">
                 <i className="bx bx-chevron-right" />
               </span>
             </div>
             <div className="page-back">
-              <EducationPage />
+              <SkillsPage />
               <span className="number-page">2</span>
               <span className="nextprev-btn back" data-page="turn-1">
                 <i className="bx bx-chevron-left" />
@@ -46,34 +44,16 @@ export function Portfolio() {
           {/* Page 3 & 4 */}
           <div className="book-page page-right turn" id="turn-2">
             <div className="page-front">
-              <ServicesPage />
+              <PortfolioPage />
               <span className="number-page">3</span>
               <span className="nextprev-btn" data-page="turn-2">
                 <i className="bx bx-chevron-right" />
               </span>
             </div>
             <div className="page-back">
-              <SkillsPage />
+              <ContactPage />
               <span className="number-page">4</span>
               <span className="nextprev-btn back" data-page="turn-2">
-                <i className="bx bx-chevron-left" />
-              </span>
-            </div>
-          </div>
-
-          {/* Page 5 & 6 */}
-          <div className="book-page page-right turn" id="turn-3">
-            <div className="page-front">
-              <PortfolioPage />
-              <span className="number-page">5</span>
-              <span className="nextprev-btn" data-page="turn-3">
-                <i className="bx bx-chevron-right" />
-              </span>
-            </div>
-            <div className="page-back">
-              <ContactPage />
-              <span className="number-page">6</span>
-              <span className="nextprev-btn back" data-page="turn-3">
                 <i className="bx bx-chevron-left" />
               </span>
               <a href="#" className="back-profile" id="back-profile">
